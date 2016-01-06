aa392cf36831840f8d13a5a1ce357bddf58cfb7d
Rahul Jain <rahulkj@gmail.com>
2016-01-06 11:08:57 -0600

    Fixed the import users issue

--- scripts/import-users.sh
+++ scripts/import-users.sh
@@ -12,7 +12,7 @@ echo "Log location is: /tmp/import-users.log"
 cf api --skip-ssl-validation $1 >> /tmp/import-users.log 2>&1
 cf login -u $2 -p $3
 
-uaac target $4 >> /tmp/import-users.log 2>&1
+uaac target --skip-ssl-validation $4 >> /tmp/import-users.log 2>&1
 uaac token client get $5 -s $6 >> /tmp/import-users.log 2>&1
 
 IFS="#"
@@ -26,12 +26,15 @@ do
         echo "SpaceName is                : $f6"
         echo "Space Permission is         : $f7"
 
-        RESPONSE=`uaac curl -H "Content-Type: application/json" -k  /Users -X POST -d '{"userName":"'"$f3"'","emails":[{"value":"'"$f2"'"}],"origin":"ldap","externalId":'$f1'}'`
+        RESPONSE=`uaac curl -H "Content-Type: application/json" -k  /Users -X POST -d '{"userName":"'"$f3"'","emails":[{"value":"'"$f2"'"}],"origin":"ldap","externalId":"'"$f1"'"}'`
+#	echo $RESPONSE
         ID=`echo $RESPONSE | grep "id" | head -3 | tail -1 | cut -d ":" -f2 | cut -d " " -f2 | cut -d "," -f1`
         echo $ID
 
-        cf curl /v2/users -d '{"guid":'$ID'}' -X POST >> /tmp/import-users.log 2>&1
-
+        IMPORT_USER_TO_CC=`cf curl /v2/users -d '{"guid":"'"$ID"'"}' -X POST`
+#	echo $IMPORT_USER_TO_CC >> /tmp/import-users.log 2>&1
+	echo $IMPORT_USER_TO_CC
+	
         IFS="|" read -a orgperms <<< "$f5"
 
         for orgperm in "${orgperms[@]}"
