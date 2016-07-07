e1e4b2319c3707a0e1cb38da85c3efcd1e0f9c86
Rahul Jain <rahulkj@gmail.com>
2016-07-07 09:31:08 -0500

    Renamed the scripts and added a new script to start/stop cf jobs in the order prescribed by product team

--- src/main/java/com/pivotal/ApplicationConstant.java
+++ src/main/java/com/pivotal/ApplicationConstant.java
@@ -10,23 +10,14 @@ public interface ApplicationConstant {
 	String PRODUCTS = "products";
 	String COMPONENTS = "components";
 	String JOBS = "jobs";
-	String IPS = "ip_assignments";
+	String IPS = "ips";
 	String PROPERTIES = "properties";
 
-	String MICROBOSH = "p-bosh";
+	String MICROBOSH = "microbosh";
 	String MICROBOSH_VCLOUD = "microbosh-vcloud";
 	String CF = "cf";
 	String CCDB = "ccdb";
 	String UAADB = "uaadb";
 	String CONSOLE_DB = "consoledb";
-	String MYSQL_DB = "mysql";
 	String NFS_SERVER = "nfs_server";
-	String GUID = "guid";
-	String AZ = "singleton_availability_zone_reference";
-	String ASSIGNMENTS = "assignments";
-	String PARTITIONS = "partitions";
-	String AZ_REF = "availability_zone_reference";
-	String VM_CREDENTIALS = "vm_credentials";
-	String JOB_REFERENCE = "job_reference";
-	
 }
--- src/test/java/com/pivotal/JsonParserTest.java
+++ src/test/java/com/pivotal/JsonParserTest.java
@@ -8,10 +8,9 @@ import java.io.InputStream;
 import org.junit.Test;
 
 public class JsonParserTest {
-	private String fileName = "/installation-1.7.json";
-	
 	@Test
 	public void validateMicroBoshDetails() {
+		String fileName = "/installation.yml";
 		try {
 			JsonParser jsonParser = new JsonParser() {
 
@@ -27,7 +26,7 @@ public class JsonParserTest {
 			String[] values = response.split("\\|");
 			assertTrue(values.length == 3);
 			assertEquals("director", values[0]);
-			assertEquals("a1dc7d4f0e324906b607", values[1]);
+			assertEquals("d98533512d91df542493", values[1]);
 			assertEquals("172.16.1.41", values[2]);
 
 		} catch (Exception e) {
@@ -36,7 +35,8 @@ public class JsonParserTest {
 	}
 
 	@Test
-	public void validateMySqlDBDetails() {
+	public void validateCcDBDetails() {
+		String fileName = "/installation.yml";
 		try {
 			JsonParser jsonParser = new JsonParser() {
 
@@ -48,13 +48,65 @@ public class JsonParserTest {
 
 			};
 
-			String response = jsonParser.getValue(fileName, ApplicationConstant.CF, ApplicationConstant.MYSQL_DB,
-					"root");
+			String response = jsonParser.getValue(fileName, ApplicationConstant.CF, ApplicationConstant.CCDB, "admin");
 			String[] values = response.split("\\|");
 			assertTrue(values.length == 3);
-			assertEquals("root", values[0]);
-			assertEquals("ad0b5f49d0424f63d3fd", values[1]);
-			assertEquals("172.16.1.51", values[2]);
+			assertEquals("admin", values[0]);
+			assertEquals("b40f5da19fbab9a20be8", values[1]);
+			assertEquals("172.16.1.48", values[2]);
+
+		} catch (Exception e) {
+			fail("This is unexpected: \n" + e);
+		}
+	}
+
+	@Test
+	public void validateUaaDBDetails() {
+		String fileName = "/installation.yml";
+		try {
+			JsonParser jsonParser = new JsonParser() {
+
+				@Override
+				InputStream getFileStream(String fileName) throws FileNotFoundException {
+					InputStream is = this.getClass().getResourceAsStream(fileName);
+					return is;
+				}
+
+			};
+
+			String response = jsonParser.getValue(fileName, ApplicationConstant.CF, ApplicationConstant.UAADB, "vcap");
+			String[] values = response.split("\\|");
+			assertTrue(values.length == 3);
+			assertEquals("vcap", values[0]);
+			assertEquals("48610c1c4e90d5bc", values[1]);
+			assertEquals("172.16.1.49", values[2]);
+
+		} catch (Exception e) {
+			fail("This is unexpected: \n" + e);
+		}
+	}
+
+	@Test
+	public void validateConsoleDBDetails() {
+		String fileName = "/installation.yml";
+		try {
+			JsonParser jsonParser = new JsonParser() {
+
+				@Override
+				InputStream getFileStream(String fileName) throws FileNotFoundException {
+					InputStream is = this.getClass().getResourceAsStream(fileName);
+					return is;
+				}
+
+			};
+
+			String response = jsonParser.getValue(fileName, ApplicationConstant.CF, ApplicationConstant.CONSOLE_DB,
+					"vcap");
+			String[] values = response.split("\\|");
+			assertTrue(values.length == 3);
+			assertEquals("vcap", values[0]);
+			assertEquals("8594ada75673625d", values[1]);
+			assertEquals("172.16.1.50", values[2]);
 
 		} catch (Exception e) {
 			fail("This is unexpected: \n" + e);
@@ -63,7 +115,7 @@ public class JsonParserTest {
 
 	@Test
 	public void validateNFSServerDetails() {
-		
+		String fileName = "/installation.yml";
 		try {
 			JsonParser jsonParser = new JsonParser() {
 
@@ -80,8 +132,8 @@ public class JsonParserTest {
 			String[] values = response.split("\\|");
 			assertTrue(values.length == 3);
 			assertEquals("vcap", values[0]);
-			assertEquals("9e07b82c2985a444", values[1]);
-			assertEquals("172.16.1.49", values[2]);
+			assertEquals("ecdd39d20528ed48", values[1]);
+			assertEquals("172.16.1.47", values[2]);
 
 		} catch (Exception e) {
 			fail("This is unexpected: \n" + e);
