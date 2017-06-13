c15e3982be68dcd23903d29632aedf7a95a762cf
Michael Both <mboth@avarteq.de>
2017-06-13 12:50:11 +0200

    add simple json parser, disable zipping all together by default

---
+++ scripts/lib/json_parser.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+USAGE="
+USAGE: #{File.basename($0)} /path/to/yaml.yml key_name_to_extract
+
+"
+
+if ARGV.length < 2 do
+  puts USAGE
+  abort "Not enough arguments."
+end
+
+file_name = ARGV[0]
+key_name = ARGV[1]
+puts JSON.parse(File.read(file_name))[key_name]
